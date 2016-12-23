aa0ac4b27d18556d22d5433d884e44a579e73d90
curiburn <sh05012@gmail.com>
2016-12-23 19:57:35 +0900

    merge simplify learn

--- py/optimizer.py
+++ py/optimizer.py
@@ -59,6 +59,7 @@ class MOABC:
         #archivesの初期化
         self.archives = []
 
+
     #シミュレータを動かす関数(並列化用)
     def simulator(self, in_bee):
         if None in in_bee["fit"].values():
@@ -80,8 +81,8 @@ class MOABC:
         
         else:
             return in_bee
-        
-
+    
+    
     #メインループ
     def learn(self):
         #収穫蜂の初期化
@@ -94,125 +95,27 @@ class MOABC:
         for i in range(self.N):
             #収穫蜂のフェーズ
             sys.stderr.write("%d th employed phase...\n" % i)
-            #各オペレータの適用
-            #   適用するオペレータがアップグレードとダウングレードのみ
-            #   適用したオペレータごとにコロニーが生成
-            tmp_colony_whole = copy.deepcopy(colony_employed)
-            tmp_colony_whole.extend([ns.flatten_both(bee, self.names_item) for bee in colony_employed])
-            
-            #全コロニーの適応値を計算
-            #   シミュレータにかける
-            #   各目的関数で結果をparse
-            p = mp.Pool(self.proc)
-            tmp_colony_whole = p.map(self.simulator, tmp_colony_whole)
-            p.close()
-            
-            #全コロニーを用いてself.archiveを更新
-            #   Qが計算されたものが帰ってくる
-            tmp_colony_whole = self.arc.update_archive_from_colony(tmp_colony_whole)
-            
-            #コロニーの連結を解除
-            #   オペレータ適用前、適用後で分ける
-            #   収穫蜂の数でぴったり割り切れるはず
-            colonies = [tmp_colony_whole[x: x + self.M_employed] for x in range(int(len(tmp_colony_whole) / self.M_employed))]
-            colony_employed = colonies[0]
-            colonies_employed_operated = colonies[1:]
-            
-            #収穫蜂のコロニーの更新
-            #   Q(i)が上がったもの(改良された解)のみオペレータを適用
-            for (j, bee) in enumerate(colony_employed):
-                #オペレータを適用した蜂を取ってくる
-                bees_operated = [colony[j] for colony in colonies_employed_operated]
-                
-                #オペレータを適用した中で最もQが高い蜂を取ってくる
-                idx_best_bee_operated = np.argmax([bee['Q'] for bee in bees_operated])
-                best_bee_operated = bees_operated[idx_best_bee_operated]
-                
-                #オペレータ適用前と比較して更新
-                #   tのリセットもする
-                if best_bee_operated['Q'] > bee['Q']:
-                    colony_employed[j] = copy.deepcopy(best_bee_operated)
-                    colony_employed[j]['t'] = 0
-                else:
-                    colony_employed[j]['t'] += 1
+            colony_employed = self._do_employed(colony_employed)
             
             #追従蜂のフェーズ
             sys.stderr.write("%d th onlooker phase...\n" % i)
-            #追従蜂の初期化
-            #   収穫蜂のコロニーのQ(i)を重みとしてランダムに選択
-            weights_colony_employed = np.array([bee['Q'] for bee in colony_employed])
-            weights_colony_employed /= sum(weights_colony_employed)
-            colony_onlooker = [\
-                copy.deepcopy(np.random.choice(colony_employed, p=weights_colony_employed))\
-                for x in range(self.M_onlooker)\
-            ]
-            
-            #各オペレータの適用
-            #   適用するオペレータがアップグレードとダウングレードと平滑化
-            #   適用したオペレータごとにコロニーが生成
-            tmp_colony_whole = copy.deepcopy(colony_onlooker)
-            tmp_colony_whole.extend([ns.upgrade(bee, "booster", self.names_item['booster']) for bee in colony_onlooker])
-            tmp_colony_whole.extend([ns.downgrade(bee, "booster", self.names_item['booster']) for bee in colony_onlooker])
-            tmp_colony_whole.extend([ns.upgrade(bee, "reducer", self.names_item['reducer']) for bee in colony_onlooker])
-            tmp_colony_whole.extend([ns.downgrade(bee, "reducer", self.names_item['reducer']) for bee in colony_onlooker])
-
-            
-            #全コロニーの適応値を計算
-            #   シミュレータにかける
-            #   各目的関数で結果をparse
-            p = mp.Pool(self.proc)
-            tmp_colony_whole = p.map(self.simulator, tmp_colony_whole)
-            p.close()
-            
-            #全コロニーを用いてself.archiveを更新
-            #   Qが計算されたものが帰ってくる
-            tmp_colony_whole = self.arc.update_archive_from_colony(tmp_colony_whole)
-            
-            #コロニーの連結を解除
-            #   オペレータ適用前、適用後で分ける
-            #   追従蜂の数でぴったり割り切れるはず
-            colonies = [tmp_colony_whole[x: (x + self.M_onlooker)] for x in range(int(len(tmp_colony_whole) / self.M_onlooker))]
-            colony_onlooker = colonies[0]
-            colonies_onlooker_operated = colonies[1:]
-            
-            #追従蜂のコロニーの更新
-            #   Q(i)が上がったもの(改良された解)のみオペレータを適用
-            for (j, bee) in enumerate(colony_onlooker):
-                #オペレータを適用した蜂を取ってくる
-                bees_operated = [colony[j] for colony in colonies_onlooker_operated]
-                
-                #オペレータを適用した中で最もQが高い蜂を取ってくる
-                idx_best_bee_operated = np.argmax([bee['Q'] for bee in bees_operated])
-                best_bee_operated = bees_operated[idx_best_bee_operated]
-                
-                #オペレータ適用前と比較して更新
-                if best_bee_operated['Q'] > bee['Q']:
-                    colony_onlooker[j] = copy.deepcopy(best_bee_operated)
+            colony_onlooker = self._do_onlooker(colony_employed)
             
             #偵察蜂のフェーズ
             sys.stderr.write("%d th scout phase...\n" % i)
-            #   LIMITに達した収穫蜂をジェネレータを使って最初期化
-            self.gen.calculate_weights()
-            for bee in colony_employed:
-                if bee['t'] >= self.LIMIT:
-                    bee['sol'] = self.gen.generate_bee_sol()
-                    bee['fit'] = {'count': None, 'item': None}
-                    bee['Q'] = np.nan
-                    bee['t'] = 0
-            
+            colony_employed = self._do_scout(colony_employed)
             
             #収穫蜂と追従蜂からもっともQが高い蜂を選び、世代の最適解としてフェロモンを更新
             sys.stderr.write("%d th pheromone updating phase...\n" % i)
             colony_all = colony_employed + colony_onlooker
-            idx_gbest_bee = np.nanargmax([bee['Q'] for bee in colony_all])
-            gbest_bee = colony_all[idx_gbest_bee]
-            self.gen.update_pheromone(gbest_bee)
+            self._update_grobal_optimum(colony_all)
             
             #現在のアーカイブを保存する
             self.archives.append(self.arc.get_archive())
 
-    #fin
-    #   self.archiveにいる蜂を出力して終わり
+    
+    #結果の保存
+    #   out_dirnameに得られた解のプロットと中身をcsvで保存
     def save_result(self, out_dirname):
         #archiveを取ってくる
         self.archived_bee = self.arc.get_archive()
@@ -241,6 +144,153 @@ class MOABC:
             
         #散布図の出力
         plt.savefig('%s/pareto_front.png' % out_dirname, dpi=600)
+
+
+    def _apply_operated_colony(self, colony, colonies_operated):
+        #収穫蜂のコロニーの更新
+        #   Q(i)が上がったもの(改良された解)のみオペレータを適用
+        for (j, bee) in enumerate(colony):
+            #オペレータを適用した蜂を取ってくる
+            bees_operated = [colony[j] for colony in colonies_operated]
+            
+            #オペレータを適用した中で最もQが高い蜂を取ってくる
+            idx_best_bee_operated = np.argmax([bee['Q'] for bee in bees_operated])
+            best_bee_operated = bees_operated[idx_best_bee_operated]
+            
+            #オペレータ適用前と比較して更新
+            #   tのリセットもする
+            if best_bee_operated['Q'] > bee['Q']:
+                colony[j] = copy.deepcopy(best_bee_operated)
+                colony[j]['t'] = 0
+            else:
+                colony[j]['t'] += 1
+        
+        return colony
+    
+    
+    #tmp_colony_wholeを分ける
+    def _separete_colony_whole(self, tmp_colony_whole, len_colony):
+        colonies = [tmp_colony_whole[x: x + len_colony] for x in range(int(len(tmp_colony_whole) / len_colony))]
+        colony = colonies[0]
+        colonies_operated = colonies[1:]
+        
+        return [colony, colonies_operated]
+    
+    
+    #オペレータの適用
+    def _apply_operator(self, colony, bee_type):
+        #収穫蜂のオペレータ
+        if bee_type == 'employed':
+            tmp_colony_whole = copy.deepcopy(colony)
+            tmp_colony_whole.extend([ns.flatten_both(bee, self.names_item) for bee in colony])
+                
+        #追従蜂のオペレータ
+        elif bee_type == 'onlooker':
+            tmp_colony_whole = copy.deepcopy(colony)
+            tmp_colony_whole.extend([ns.upgrade(bee, "booster", self.names_item['booster']) for bee in colony])
+            tmp_colony_whole.extend([ns.downgrade(bee, "booster", self.names_item['booster']) for bee in colony])
+            tmp_colony_whole.extend([ns.upgrade(bee, "reducer", self.names_item['reducer']) for bee in colony])
+            tmp_colony_whole.extend([ns.downgrade(bee, "reducer", self.names_item['reducer']) for bee in colony])
+            
+        else:
+            sys.exit('bee_type is not vaild(%s)' % bee_type)
+        
+        return tmp_colony_whole
+    
+    
+    #収穫蜂のフェーズ
+    def _do_employed(self, colony_employed):
+        #収穫蜂のフェーズ
+        #各オペレータの適用
+        #   適用するオペレータがアップグレードとダウングレードのみ
+        #   適用したオペレータごとにコロニーが生成
+        tmp_colony_whole = self._apply_operator(colony_employed, 'employed')
+        
+        #全コロニーの適応値を計算
+        #   シミュレータにかける
+        #   各目的関数で結果をparse
+        p = mp.Pool(self.proc)
+        tmp_colony_whole = p.map(self.simulator, tmp_colony_whole)
+        p.close()
+        
+        #全コロニーを用いてself.archiveを更新
+        #   Qが計算されたものが帰ってくる
+        tmp_colony_whole = self.arc.update_archive_from_colony(tmp_colony_whole)
+        
+        #コロニーの連結を解除
+        #   オペレータ適用前、適用後で分ける
+        #   収穫蜂の数でぴったり割り切れるはず
+        colony_separated = self._separete_colony_whole(tmp_colony_whole, self.M_employed)
+        colony_employed = colony_separated[0]
+        colonies_employed_operated = colony_separated[1]
+        
+        #収穫蜂のコロニーの更新
+        colony_employed = self._apply_operated_colony(colony_employed, colonies_employed_operated)
+        
+        return colony_employed
+    
+    
+    #追従蜂のフェーズ
+    def _do_onlooker(self, colony_employed):
+        #追従蜂のフェーズ
+        #追従蜂の初期化
+        #   収穫蜂のコロニーのQ(i)を重みとしてランダムに選択
+        weights_colony_employed = np.array([bee['Q'] for bee in colony_employed])
+        weights_colony_employed /= sum(weights_colony_employed)
+        colony_onlooker = [\
+            copy.deepcopy(np.random.choice(colony_employed, p=weights_colony_employed))\
+            for x in range(self.M_onlooker)\
+        ]
+        
+        #各オペレータの適用
+        #   適用するオペレータがアップグレードとダウングレードと平滑化
+        #   適用したオペレータごとにコロニーが生成
+        tmp_colony_whole = self._apply_operator(colony_onlooker, 'onlooker')
+        
+        #全コロニーの適応値を計算
+        #   シミュレータにかける
+        #   各目的関数で結果をparse
+        p = mp.Pool(self.proc)
+        tmp_colony_whole = p.map(self.simulator, tmp_colony_whole)
+        p.close()
+        
+        #全コロニーを用いてself.archiveを更新
+        #   Qが計算されたものが帰ってくる
+        tmp_colony_whole = self.arc.update_archive_from_colony(tmp_colony_whole)
+        
+        #コロニーの連結を解除
+        #   オペレータ適用前、適用後で分ける
+        #   追従蜂の数でぴったり割り切れるはず
+        colony_separated = self._separete_colony_whole(tmp_colony_whole, self.M_onlooker)
+        colony_onlooker = colony_separated[0]
+        colonies_onlooker_operated = colony_separated[1]
+        
+        #追従蜂のコロニーの更新
+        colony_onlooker = self._apply_operated_colony(colony_onlooker, colonies_onlooker_operated)
+        
+        return colony_onlooker
+
+    
+    #偵察蜂のフェーズ
+    #   LIMITに達した収穫蜂をジェネレータを使って最初期化
+    def _do_scout(self, colony):
+        self.gen.calculate_weights()
+        for bee in colony:
+            if bee['t'] >= self.LIMIT:
+                bee['sol'] = self.gen.generate_bee_sol()
+                bee['fit'] = {'count': None, 'item': None}
+                bee['Q'] = np.nan
+                bee['t'] = 0
+    
+        return colony
+    
+    
+    #世代の最適解の更新
+    def _update_grobal_optimum(self, colony_all):
+        idx_gbest_bee = np.nanargmax([bee['Q'] for bee in colony_all])
+        gbest_bee = colony_all[idx_gbest_bee]
+        self.gen.update_pheromone(gbest_bee)
+
 
 if __name__ == '__main__':
     #引数の処理
